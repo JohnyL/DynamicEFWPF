a6a658d59baf29bedb292249c42fe5aef7dfa6db
Eugene <sektorLP@yandex.ru>
2017-09-17 22:02:43 +0300

    Add files via upload

---
+++ Database/Context.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicEFWPF
+{
+	public class TestContext : DbContext
+	{
+		public DbSet<Strategy> Strategies { get; set; }
+		public DbSet<Operator> Operators { get; set; }
+		public DbSet<OrderState> OrderStates { get; set; }
+
+		public TestContext(string nameOrConnectionString) : base(nameOrConnectionString)
+		{
+			Database.Initialize(true);
+		}
+
+	}
+
+	[Table("Strategy")]
+	public class Strategy
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+
+	[Table("Operator")]
+	public class Operator
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+
+	[Table("OrderState")]
+	public class OrderState
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+
+}
---
+++ Database/Script.sql
@@ -0,0 +1,34 @@
+﻿use master;
+go
+
+drop database if exists Test;
+go
+
+create database Test;
+go
+
+use Test;
+go
+
+create table dbo.Strategy
+(
+	Id     int identity not null primary key,
+	[Name] varchar(500)
+);
+
+create table dbo.Operator
+(
+	Id     int identity not null primary key,
+	[Name] varchar(500)
+);
+
+create table dbo.OrderState
+(
+	Id     int identity not null primary key,
+	[Name] varchar(500)
+);
+
+-- Insert dummy values
+insert into dbo.Strategy values ('Strategy1'), ('Strategy2'), ('Strategy3');
+insert into dbo.Operator values ('Operator1'), ('Operator2'), ('Operator3');
+insert into dbo.OrderState values ('OrderState1'), ('OrderState2'), ('OrderState3');
